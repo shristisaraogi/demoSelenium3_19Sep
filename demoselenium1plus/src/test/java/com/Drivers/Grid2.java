08f39963b8bf26aec2889e2a6789d5016e3eebbb
A07208trng_B4A.04.28 <smitar123>
2018-09-19 15:04:11 +0530

    New changes

---
+++ demoselenium1plus/src/test/java/com/Drivers/Grid2.java
@@ -0,0 +1,42 @@
+package com.Drivers;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Grid2 {
+
+	WebDriver driver;
+	String browsertest="chrome";
+	
+	@BeforeMethod
+	public void openbrowser() throws MalformedURLException{
+		
+		String nodeURL="http://10.20.88.130:5588/wd/hub"; // Node IP Address
+		
+		DesiredCapabilities caps=DesiredCapabilities.chrome();
+		caps.setBrowserName("chrome");
+		caps.setPlatform(Platform.WIN10);
+		
+		driver=new RemoteWebDriver(new URL(nodeURL), caps);
+		
+		
+		driver.manage().window().maximize();
+		//IMPLICIT WAIT
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+			}
+	
+	@Test
+	public void testcase(){
+		driver.get("http://www.flipkart.com");
+		System.out.println(driver.getTitle());
+
+}
+}
